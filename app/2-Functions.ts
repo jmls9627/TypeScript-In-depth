671a08fc048329dc61f6d1983210d987c38fb78c
jmls9627 <jmls9627@gmail.com>
2020-03-30 11:46:02 -0400

    Functions Part2

--- app/2-Functions.ts
+++ app/2-Functions.ts
@@ -22,7 +22,59 @@ function CreateCustomer(name: string, age?: number, city?: string):void{
     }
 }
 
-biographyBooks.forEach(title=>console.log(title));
+/////////////////
+function CheckOutBooks(customer:string, ...bookIDs:number[]):string[]{
+    
+    console.log("Checking out books for " + customer);
+  
+     let booksCheckedOut:string[]=[];
+  
+  for(let id of bookIDs){
+      let book=GetBookID(id);
+      if(book.available){
+          booksCheckedOut.push(book.title);
+      }
+  }
+return booksCheckedOut;
+}
+
+let myBook:string[]= CheckOutBooks("thorne",1,3,4);
+myBook.forEach(title=> console.log(title));
+
+
+function GetTitles(author:string):string[];
+function GetTitles(available:boolean):string[];
+function GetTitles(bookProperty: any):string[]{
+    
+    const allBooks=GetAllOfBooks();
+    const foundTitles: string[]=[];
+    
+    if(typeof bookProperty=='string'){
+        //get all books by a particular author
+        for(let book of allBooks){
+            if(book.author===bookProperty){
+                foundTitles.push(book.title);
+            }
+        }
+    }
+else if( typeof bookProperty=='boolean'){
+    //get all books based on specified availability
+    for(let book of allBooks){
+        if(book.available===bookProperty){
+            foundTitles.push(book.title);
+        }
+    }
+}
+return foundTitles;
+}
+
+let Books=GetTitles('string|bool');
+Books.forEach(title=>console.log(title));
+
+
+
+//LogFirstAvailable();
+//biographyBooks.forEach(title=>console.log(title));
 
 
 //CreateCustomer("Pepone",23,"havana");
